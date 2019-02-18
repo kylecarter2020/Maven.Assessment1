5eceeffe456f8bbda0d93fca22aef2709c7559d1
Kyle Carter <kylecarter@zipcoders-MacBook-Pro-9.local>
2019-02-18 16:19:57 -0500

    part 3 pet and pet owner complete, most tests passing

--- src/main/java/com/zipcodewilmington/assessment1/part3/Pet.java
+++ src/main/java/com/zipcodewilmington/assessment1/part3/Pet.java
@@ -59,13 +59,13 @@ public abstract class Pet implements Animal {
      * ensure this instance of `Pet` is added to the owner's composite `pets` list
      */
     public void setOwner(PetOwner newPetOwner) {
-
+        this.owner = newPetOwner;
     }
 
     /**
      * @return PetOwner object whose composite `pets` collection contains this Pet instance
      */
     public PetOwner getOwner() {
-        return null;
+        return this.owner;
     }
 }
--- src/main/java/com/zipcodewilmington/assessment1/part3/PetOwner.java
+++ src/main/java/com/zipcodewilmington/assessment1/part3/PetOwner.java
@@ -1,27 +1,40 @@
 package com.zipcodewilmington.assessment1.part3;
 
+import java.util.ArrayList;
+
 /**
  * Created by leon on 2/16/18.
  */
 public class PetOwner {
+    protected String name;
+    protected int age;
+    protected ArrayList<Pet> petList = new ArrayList<>();
     /**
      * @param name name of the owner of the Pet
      * @param pets array of Pet object
      */
     public PetOwner(String name, Pet... pets) {
+        this.name = name;
+
+        if(!(pets == null)) {
+            for (Pet pet : pets) {
+                this.petList.add(pet);
+            }
+        }
     }
 
     /**
      * @param pet pet to be added to the composite collection of Pets
      */
     public void addPet(Pet pet) {
+        this.petList.add(pet);
     }
 
     /**
      * @param pet pet to be removed from the composite collection Pets
      */
     public void removePet(Pet pet) {
-
+        this.petList.remove(pet);
     }
 
     /**
@@ -29,14 +42,32 @@ public class PetOwner {
      * @return true if I own this pet
      */
     public Boolean isOwnerOf(Pet pet) {
-        return null;
+        boolean result = false;
+
+        for (Pet p : this.petList
+             ) {
+            if(p.equals(pet))
+            {
+                result = true;
+            }
+        }
+        return result;
     }
 
     /**
      * @return the age of the Pet object whose age field is the lowest amongst all Pets in this class
      */
     public Integer getYoungetPetAge() {
-        return null;
+        int youngest = 100;
+
+        for (Pet pet : this.petList
+             ) {
+            if(pet.getAge() < youngest)
+            {
+                youngest = pet.getAge();
+            }
+        }
+        return youngest;
     }
 
 
@@ -46,7 +77,16 @@ public class PetOwner {
      * @return the age of the Pet object whose age field is the highest amongst all Pets in this class
      */
     public Integer getOldestPetAge() {
-        return null;
+        int oldest = 0;
+
+        for (Pet pet : this.petList
+             ) {
+            if(pet.getAge() > oldest)
+            {
+                oldest = pet.getAge();
+            }
+        }
+        return oldest;
     }
 
 
@@ -54,27 +94,42 @@ public class PetOwner {
      * @return the sum of ages of Pet objects stored in this class divided by the number of Pet object
      */
     public Float getAveragePetAge() {
-        return null;
+        float avg = 0;
+        int ageSum = 0;
+
+        for (Pet pet : this.petList
+             ) {
+            ageSum += pet.getAge();
+        }
+        avg = ageSum/getNumberOfPets();
+
+        return avg;
     }
 
     /**
      * @return the number of Pet objects stored in this class
      */
     public Integer getNumberOfPets() {
-        return null;
+        int numPets = 0;
+
+        for (Pet pet: this.petList
+             ) {
+            numPets++;
+        }
+        return numPets;
     }
 
     /**
      * @return the name property of the Pet
      */
     public String getName() {
-        return null;
+        return this.name;
     }
 
     /**
      * @return array representation of animals owned by this PetOwner
      */
     public Pet[] getPets() {
-        return null;
+        return petList.toArray(new Pet[petList.size()]);
     }
 }
